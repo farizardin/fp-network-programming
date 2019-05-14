2bafc6c88ffcf93ca299d9210a22458ebdfa9d4e
Fariz <fariz.ardin@gmail.com>
2019-05-14 22:53:27 +0700

    pull

--- chat-cli.py
+++ chat-cli.py
@@ -9,84 +9,87 @@ TARGET_PORT = 8889
 class ChatClient:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_address = (TARGET_IP,TARGET_PORT)
+        self.server_address = (TARGET_IP, TARGET_PORT)
         self.sock.connect(self.server_address)
-        self.tokenid=""
-    def proses(self,cmdline):
-	j=cmdline.split(" ")
-	try:
-	    command=j[0].strip()
-	    if (command=='auth'):
-		    username=j[1].strip()
-		    password=j[2].strip()
-            # return self.user.auth(username,password)
-        return self.login(username,password)
-        
-        elif (command == 'create'):
-            return self.buat('test','test')
+        self.tokenid = ""
 
-	    elif (command=='send'):
-		    usernameto = j[1].strip()
-            message=""
-            for w in j[2:]:
-                message="{} {}" . format(message,w)
-		    return self.sendmessage(usernameto,message)
-        
-        elif (command=='inbox'):
-            return self.inbox()
+    def proses(self, cmdline):
+        j = cmdline.split(" ")
+        try:
+            command = j[0].strip()
+            if (command == 'auth'):
+                if (self.tokenid == ""):
+                    username = j[1].strip()
+                    password = j[2].strip()
+                    return self.login(username, password)
+                else:
+                    return "tidak bisa login"
+            elif (command == 'send'):
+                usernameto = j[1].strip()
+                message = ""
+                for w in j[2:]:
+                    message = "{} {}" . format(message, w)
+                return self.sendmessage(usernameto, message)
+            elif (command == 'inbox'):
+                return self.inbox()
             elif (command == 'logout'):
-                self.tokenid=""
-	    else:
-		return "*Maaf, command tidak benar"
-	except IndexError:
-	    return "-Maaf, command tidak benar"
-    def sendstring(self,string):
+                self.tokenid = ""
+            elif (command == 'create'):
+                return self.buat('test','test')
+            else:
+                return "*Maaf, command tidak benar"
+        except IndexError:
+            return "-Maaf, command tidak benar"
+
+    def sendstring(self, string):
         try:
             self.sock.sendall(string)
             receivemsg = ""
             while True:
                 data = self.sock.recv(10)
                 if (data):
-                    receivemsg = "{}{}" . format(receivemsg,data)
-                    if receivemsg[-4:]=="\r\n\r\n":
+                    receivemsg = "{}{}" . format(receivemsg, data)
+                    if receivemsg[-4:] == "\r\n\r\n":
                         return json.loads(receivemsg)
         except:
             self.sock.close()
-    def login(self,username,password):
-        return self.user.auth(username,password)
-        # string="auth {} {} \r\n" . format(username,password)
-        # result = self.sendstring(string)
-        # if result['status']=='OK':
-        #     self.tokenid=result['tokenid']
-        #     return "username {} logged in, token {} " .format(username,self.tokenid)
-        # else:
-        #     return "Error, {}" . format(result['message'])
-    def buat(self,username,password):
-        return self.user.createUser(username, password)
-    def sendmessage(self,usernameto="xxx",message="xxx"):
-        if (self.tokenid==""):
-            return "Error, not authorized"
-        string="send {} {} {} \r\n" . format(self.tokenid,usernameto,message)
+
+    def login(self, username, password):
+        string="auth {} {} \r\n" . format(username,password)
         result = self.sendstring(string)
         if result['status']=='OK':
+            self.tokenid=result['tokenid']
+            return "username {} logged in, token {} " .format(username,self.tokenid)
+        else:
+            return "Error, {}" . format(result['message'])
+    
+    def buat(self,username,password):
+        return self.user.createUser(username,password)
+
+    def sendmessage(self, usernameto="xxx", message="xxx"):
+        if (self.tokenid == ""):
+            return "Error, not authorized"
+        string = "send {} {} {} \r\n" . format(
+            self.tokenid, usernameto, message)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
             return "message sent to {}" . format(usernameto)
         else:
             return "Error, {}" . format(result['message'])
+
     def inbox(self):
-        if (self.tokenid==""):
+        if (self.tokenid == ""):
             return "Error, not authorized"
-        string="inbox {} \r\n" . format(self.tokenid)
+        string = "inbox {} \r\n" . format(self.tokenid)
         result = self.sendstring(string)
-        if result['status']=='OK':
+        if result['status'] == 'OK':
             return "{}" . format(json.dumps(result['messages']))
         else:
             return "Error, {}" . format(result['message'])
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     cc = ChatClient()
     while True:
         cmdline = raw_input("Command {}:" . format(cc.tokenid))
         print cc.proses(cmdline)
-
