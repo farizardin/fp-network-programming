053876a711be1d5cce55ccd80ca51af209548045
Fariz <fariz.ardin@gmail.com>
2019-05-23 19:22:37 +0700

    tugas 5

---
+++ Tugas5/perftest.sh
@@ -0,0 +1,5 @@
+#ab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testingab -n jumlah_request -c jumlah_konkuren http://localhost:8887/testing.txt
+
+
+ab -n 20000 -c 1 http://localhost:8885/testing.txt
+
